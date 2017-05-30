7bf54a352776eae25cec18995fbf4a919a5619a4
soyoung <soyoungp02@gmail.com>
2017-05-30 15:59:54 +0900

    git test

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -3,4 +3,6 @@ package kr.or.connect.todo.persistence;
 public class TodoSqls {
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
+	private static final String COUNT_BOOK = 
+			"SELECT COUNT(*) FROM book";
 }
