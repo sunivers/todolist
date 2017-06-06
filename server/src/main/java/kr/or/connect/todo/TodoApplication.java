0207c5bdb2007e97d5a2755c1dd56ef0073341e7
soyoung <soyoungp02@gmail.com>
2017-06-07 00:00:46 +0900

    DB 수정

--- server/src/main/java/kr/or/connect/todo/TodoApplication.java
+++ server/src/main/java/kr/or/connect/todo/TodoApplication.java
@@ -2,8 +2,6 @@ package kr.or.connect.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -5,7 +5,7 @@ public class TodoSqls {
 			"SELECT COUNT(*) FROM todo";
 	
 	static final String SELECT_BY_ID = 
-			"SELECT id, text, done FROM todo where id = :id";
+			"SELECT * FROM todo where id = :id";
 	
 	static final String SELECT_ALL =
 			"SELECT * FROM todo";
@@ -14,7 +14,7 @@ public class TodoSqls {
 			"DELETE FROM todo WHERE id = :id";
 	
 	static final String UPDATE = "UPDATE todo SET\n"
-										+ "text = :text,"
-										+ "done = :done\n"
+										+ "todo = :text,"
+										+ "completed = :done\n"
 										+ "WHERE id = :id";
 }
